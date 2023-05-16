77333b89e48812ef93ce1e755f3b2d3e442385e1
Shazia456 <128918032+Shazia456@users.noreply.github.com>
2023-05-16 10:31:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,111 @@
+// // // // // let a = 3;
+// // // // // let b = 5;
+// // // // // let sum = (a+b);
+// // // // // document.write("Sum of 3 and 5 is 8");
+
+// // // // // //   Ques#2
+// // // // //   let c = 4;
+// // // // //   let d = 5;
+// // // // //   let subtraction = c-d;
+// // // // // //   multiplication
+
+// // // // // var num1 = 5;
+// // // // // var num2 = 7;
+// // // // // var multiplication = num1*num2;
+
+// // // // // //   division
+// // // // // var num3 = 125;
+// // // // // var num4 = 5;
+// // // // // var division = num3/num4; 
+
+
+// // // // // //   modulus
+
+// // // // // var modulus = 55%6;
+
+// // // // // // ques3
+// // // // var value;
+// // // //  document.write("value after vairable declaration is " + value + "<br>");
+// // // //  var number = 9;
+// // // //  document.write("initial value" + number + "<br>" );
+// // // //  var increment = ++number;
+// // // //  document.write("value after increment is " + increment + "<br>");
+// // // //  var addition = increment + 5;
+// // // //  document.write("value after addition is " + addition + "<br>");
+// // // //  var decrement = --addition;
+// // // //  document.write("value after decrement is" + decrement + "<br>");
+
+// // // // //  ques4
+// // // // var ticketPrice = 600;
+// // // // var buying = 5;
+// // // // var fiveTicketPrice = ticketPrice*buying;
+// // // // document.write("total cost to buy" + buying +"tickets to a movie is " + fiveTicketPrice);
+
+// // // // // ques5
+// // // var table = 4;
+// // // var num1 =1;
+// // // var num2 =2;
+// // // var num3 =3;
+// // // var num4 =4;
+// // // var num5 =5;
+// // // var num6 =6;
+// // // var num7 =7;
+// // // var num8 =8;
+// // // var num9 =9;
+// // // var num10 =10;
+// // // document.write("<b>"+ "Table of" + table + "<br>");
+// // // document.write(table + "x" + 1 + "=" + table*num1 + "<br>");
+// // // document.write(table + "x" + 2 + "=" + table*num2 + "<br>");
+// // // document.write(table + "x" + 3 + "=" + table*num3 + "<br>");
+// // // document.write(table + "x" + 4 + "=" + table*num4 + "<br>");
+// // // document.write(table + "x" + 5 + "=" + table*num5 + "<br>");
+// // // document.write(table + "x" + 6 + "=" + table*num6 + "<br>");
+// // // document.write(table + "x" + 7 + "=" + table*num7 + "<br>");
+// // // document.write(table + "x" + 8 + "=" + table*num8 + "<br>");
+// // // document.write(table + "x" + 9 + "=" + table*num9 + "<br>");
+// // // document.write(table + "x" + 10 + "=" + table*num10 + "<br>");
+
+
+// // // // ques6
+// //  var celsius = +prompt("enter temperature in celsuis");
+// //  var fahrenheit = +prompt("enter temperature in fahrenheit");
+// //  document.write("temperature" + celsius + "<sup>O</sup>" + ((celsius*9/5)+32) +"<sup>O</sup>"+ "<br>");
+
+// // qus7
+
+
+
+// var priceOfItem1 = +prompt("price of item");
+// var quantityOfItem1 = +prompt("quantity of item");
+// var priceOfItem2 = +prompt("price of item");
+// var quantityOfItem2 = +prompt("quantity of item");
+// var shippingCharges =100;
+// document.write(" <b> shopping cart" + "<br>");
+// document.write("price of item 1 is" + priceOfItem1 + "<br>");
+// document.write("quantity of item 1 is" + quantityOfItem1 + "<br>" );
+// document.write("price of item 2 is" + priceOfItem2 + "<br>");
+// document.write("quantity of item 2 is" + quantityOfItem2 + "<br>" );
+// document.write("shipping charges" + shippingCharges + "<br>");
+// document.write("total cost of your order is" + (priceOfItem1*quantityOfItem1 + priceOfItem2*quantityOfItem2 + shippingCharges) );
+
+// ques9
+var $dollars = 5;
+var riyal = 30;
+var convertIntoPakistaniRupees = $dollars*104.80 + riyal*28;
+document.write("<br> Currency in PKR" + "<br>");
+document.write("total currency in PkR" + convertIntoPakistaniRupees);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
